29425fa19ccf6e44fe34c050260f319b5e405748
edelkas <edlucasma@gmail.com>
2020-04-08 04:43:09 +0200

    nprofile patcher - score patching implemented, total scores table implemented

--- nprofile_patcher.rb
+++ nprofile_patcher.rb
@@ -1,9 +1,5 @@
-# To use, install the terminal-table gem:
-# Linux: sudo gem install terminal-table
-
 require 'open-uri'
 require 'json'
-require 'terminal-table'
 
 # Constants
 $o_l = "80D320".to_i(16)
@@ -44,6 +40,29 @@ class String
   def underline;      "\e[4m#{self}\e[24m" end
   def blink;          "\e[5m#{self}\e[25m" end
   def reverse_color;  "\e[7m#{self}\e[27m" end
+  def plain;      self.gsub /\e\[\d+m/, "" end
+end
+
+# To use this you need to send either a matrix or :sep
+def make_table(rows, sep_x = "=", sep_y = "|", sep_i = "x")
+  text_rows = rows.select{ |r| r.is_a?(Array) }
+  count = text_rows.map(&:size).max
+  rows.each{ |r| if r.is_a?(Array) then r << "" while r.size < count end }
+  widths = (0..count - 1).map{ |c| text_rows.map{ |r| r[c].to_s.length }.max }
+  sep = widths.map{ |w| sep_i + sep_x * (w + 2) }.join + sep_i + "\n"
+  table = sep.dup
+  rows.each{ |r|
+    if r == :sep
+      table << sep
+    else
+      r.each_with_index{ |s, i|
+        table << sep_y + " " + (s.is_a?(Numeric) ? s.to_s.rjust(widths[i], " ") : s.to_s.ljust(widths[i], " ")) + " "
+      }
+      table << sep_y + "\n"
+    end
+  }
+  table << sep
+  return table
 end
 
 def r_l(i) ($o_l + i * $d..$o_l + (i + 1) * $d - 1) end
@@ -75,6 +94,33 @@ def _unpack(bytes)
   bytes.unpack('H*')[0].scan(/../).reverse.join.to_i(16)
 end
 
+def update_scores
+  $lvls = {
+    :scored => {
+      :locked => $raw_l.select{ |s| s[0] < 1000 * $l && s[2] == 0 },
+      :unlocked => $raw_l.select{ |s| s[0] < 1000 * $l && s[2] == 1 },
+      :beaten => $raw_l.select{ |s| s[0] < 1000 * $l && s[2] == 2 }
+    },
+    :unscored => {
+      :locked => $raw_l.select{ |s| s[0] > 1000 * $l && s[2] == 0 },
+      :unlocked => $raw_l.select{ |s| s[0] > 1000 * $l && s[2] == 1 },
+      :beaten => $raw_l.select{ |s| s[0] > 1000 * $l && s[2] == 2 }
+    }
+  }
+  $eps = {
+    :scored => {
+      :locked => $raw_e.select{ |s| s[0] < 1000 * $l && s[2] == 0 },
+      :unlocked => $raw_e.select{ |s| s[0] < 1000 * $l && s[2] == 1 },
+      :beaten => $raw_e.select{ |s| s[0] < 1000 * $l && s[2] == 2 }
+    },
+    :unscored => {
+      :locked => $raw_e.select{ |s| s[0] > 1000 * $l && s[2] == 0 },
+      :unlocked => $raw_e.select{ |s| s[0] > 1000 * $l && s[2] == 1 },
+      :beaten => $raw_e.select{ |s| s[0] > 1000 * $l && s[2] == 2 }
+    }
+  }
+end
+
 def parse_savefile
   # Map episode and level names to IDs
   ids_e = (0..$n_e - 1).to_a.map{ |e| "" }
@@ -104,30 +150,7 @@ def parse_savefile
   }
 
   # Gather relevant stats
-  $lvls = {
-    :scored => {
-      :locked => $raw_l.select{ |s| s[0] < 1000 * $l && s[2] == 0 },
-      :unlocked => $raw_l.select{ |s| s[0] < 1000 * $l && s[2] == 1 },
-      :beaten => $raw_l.select{ |s| s[0] < 1000 * $l && s[2] == 2 }
-    },
-    :unscored => {
-      :locked => $raw_l.select{ |s| s[0] > 1000 * $l && s[2] == 0 },
-      :unlocked => $raw_l.select{ |s| s[0] > 1000 * $l && s[2] == 1 },
-      :beaten => $raw_l.select{ |s| s[0] > 1000 * $l && s[2] == 2 }
-    }
-  }
-  $eps = {
-    :scored => {
-      :locked => $raw_e.select{ |s| s[0] < 1000 * $l && s[2] == 0 },
-      :unlocked => $raw_e.select{ |s| s[0] < 1000 * $l && s[2] == 1 },
-      :beaten => $raw_e.select{ |s| s[0] < 1000 * $l && s[2] == 2 }
-    },
-    :unscored => {
-      :locked => $raw_e.select{ |s| s[0] > 1000 * $l && s[2] == 0 },
-      :unlocked => $raw_e.select{ |s| s[0] > 1000 * $l && s[2] == 1 },
-      :beaten => $raw_e.select{ |s| s[0] > 1000 * $l && s[2] == 2 }
-    }
-  }
+  update_scores
   return true
 end
 
@@ -178,61 +201,67 @@ def print_errors
 end
 
 def print_table
-  table = Terminal::Table.new do |t|
-    t.style = {border_x: "=", border_i: "x"}
-    t << ["LEVELS", "Locked", "Unlocked", "Beaten", "Total"]
-    t << :separator
-    t << [
-      "Scored",
-      $lvls[:scored][:locked].size,
-      $lvls[:scored][:unlocked].size,
-      $lvls[:scored][:beaten].size,
-      $lvls[:scored].reduce(0){ |sum, s| sum += s[1].size }
-    ]
-    t << [
-      "Unscored",
-      $lvls[:unscored][:locked].size,
-      $lvls[:unscored][:unlocked].size,
-      $lvls[:unscored][:beaten].size,
-      $lvls[:unscored].reduce(0){ |sum, s| sum += s[1].size }
-    ]
-    t << [
-      "Total",
-      $lvls[:scored][:locked].size + $lvls[:unscored][:locked].size,
-      $lvls[:scored][:unlocked].size + $lvls[:unscored][:unlocked].size,
-      $lvls[:scored][:beaten].size + $lvls[:unscored][:beaten].size,
-      $lvls[:scored].reduce(0){ |sum, s| sum += s[1].size } + $lvls[:unscored].reduce(0){ |sum, s| sum += s[1].size }
-    ]
-    t << :separator
-    t << ["EPISODES", "Locked", "Unlocked", "Beaten", "Total"]
-    t << :separator
-    t << [
-      "Scored",
-      $eps[:scored][:locked].size,
-      $eps[:scored][:unlocked].size,
-      $eps[:scored][:beaten].size,
-      $eps[:scored].reduce(0){ |sum, s| sum += s[1].size }
-    ]
-    t << [
-      "Unscored",
-      $eps[:unscored][:locked].size,
-      $eps[:unscored][:unlocked].size,
-      $eps[:unscored][:beaten].size,
-      $eps[:unscored].reduce(0){ |sum, s| sum += s[1].size }
-    ]
-    t << [
-      "Total",
-      $eps[:scored][:locked].size + $eps[:unscored][:locked].size,
-      $eps[:scored][:unlocked].size + $eps[:unscored][:unlocked].size,
-      $eps[:scored][:beaten].size + $eps[:unscored][:beaten].size,
-      $eps[:scored].reduce(0){ |sum, s| sum += s[1].size } + $eps[:unscored].reduce(0){ |sum, s| sum += s[1].size }
-    ]
-  end
-  puts table
+  t = []
+  t << ["LEVELS", "Locked", "Unlocked", "Beaten", "Total"]
+  t << :sep
+  t << [
+    "Scored",
+    $lvls[:scored][:locked].size,
+    $lvls[:scored][:unlocked].size,
+    $lvls[:scored][:beaten].size,
+    $lvls[:scored].reduce(0){ |sum, s| sum += s[1].size }
+  ]
+  t << [
+    "Unscored",
+    $lvls[:unscored][:locked].size,
+    $lvls[:unscored][:unlocked].size,
+    $lvls[:unscored][:beaten].size,
+    $lvls[:unscored].reduce(0){ |sum, s| sum += s[1].size }
+  ]
+  t << [
+    "Total",
+    $lvls[:scored][:locked].size + $lvls[:unscored][:locked].size,
+    $lvls[:scored][:unlocked].size + $lvls[:unscored][:unlocked].size,
+    $lvls[:scored][:beaten].size + $lvls[:unscored][:beaten].size,
+    $lvls[:scored].reduce(0){ |sum, s| sum += s[1].size } + $lvls[:unscored].reduce(0){ |sum, s| sum += s[1].size }
+  ]
+  t << :sep
+  t << ["EPISODES", "Locked", "Unlocked", "Beaten", "Total"]
+  t << :sep
+  t << [
+    "Scored",
+    $eps[:scored][:locked].size,
+    $eps[:scored][:unlocked].size,
+    $eps[:scored][:beaten].size,
+    $eps[:scored].reduce(0){ |sum, s| sum += s[1].size }
+  ]
+  t << [
+    "Unscored",
+    $eps[:unscored][:locked].size,
+    $eps[:unscored][:unlocked].size,
+    $eps[:unscored][:beaten].size,
+    $eps[:unscored].reduce(0){ |sum, s| sum += s[1].size }
+  ]
+  t << [
+    "Total",
+    $eps[:scored][:locked].size + $eps[:unscored][:locked].size,
+    $eps[:scored][:unlocked].size + $eps[:unscored][:unlocked].size,
+    $eps[:scored][:beaten].size + $eps[:unscored][:beaten].size,
+    $eps[:scored].reduce(0){ |sum, s| sum += s[1].size } + $eps[:unscored].reduce(0){ |sum, s| sum += s[1].size }
+  ]
+  puts make_table(t)
 end
 
-def url(id, i)
-  "https://dojo.nplusplus.ninja/prod/steam/get_scores?steam_id=%s&steam_auth=&level_id=%s" % [id, i]
+def download(id, i, ep = false)
+  att ||= 0
+  ret = open("https://dojo.nplusplus.ninja/prod/steam/get_scores?steam_id=#{id}&steam_auth=&#{ep ? "episode" : "level"}_id=#{i}").read
+  if ret == '-1337'
+    print("\nThe Steam ID is not active, please open N++ and patch again.\n")
+    return -1
+  end
+  return JSON.parse(ret)['userInfo']
+rescue
+  (att += 1) < 2 ? retry : (print("Server down, retry again later."); return -1)
 end
 
 def patch_scores(types = [])
@@ -240,37 +269,120 @@ def patch_scores(types = [])
   file = File.binread("nprofile")
   print("Introduce your SteamID64: ")
   id = STDIN.gets.chomp.to_i
+  success = true
   t = Time.now
 
   if types.empty? || types.include?(:lvl_scored_locked)
     print "Patching scored locked levels..."
     $lvls[:scored][:locked].each{ |s| file[combine_r(r_l(s[3]), 20..23)] = _pack(2,4) }
+    File.binwrite("nprofile", file)
     print " patched.\n"
   end
   if types.empty? || types.include?(:lvl_scored_unlocked)
     print "Patching scored unbeaten levels..."
     $lvls[:scored][:unlocked].each{ |s| file[combine_r(r_l(s[3]), 20..23)] = _pack(2,4) }
+    File.binwrite("nprofile", file)
     print " patched.\n"
   end
   if types.empty? || types.include?(:ep_scored_locked)
     print "Patching scored locked episodes..."
     $eps[:scored][:locked].each{ |s| file[combine_r(r_l(s[3]), 20..23)] = _pack(2,4) }
+    File.binwrite("nprofile", file)
     print " patched.\n"
   end
   if types.empty? || types.include?(:ep_scored_unlocked)
     print "Patching scored unbeaten episodes..."
     $eps[:scored][:unlocked].each{ |s| file[combine_r(r_l(s[3]), 20..23)] = _pack(2,4) }
+    File.binwrite("nprofile", file)
     print " patched.\n"
   end
   if types.empty? || types.include?(:lvl_unscored_beaten)
     print "Patching unscored beaten levels..."
-    #$lvls[:unscored][:beaten].each{ |s| file[combine_r(r_l(s[3]), 20..23)] = _pack(2,4); print "." }
-    print " patched.\n"
+    $lvls[:unscored][:beaten].each{ |s|
+      ret = download(id, s[3])
+      if ret == -1 || !success
+        success = false
+        break
+      else
+        next if ret.nil?
+        file[combine_r(r_l(s[3]), 36..39)] = _pack(ret['my_score']    , 4)
+        file[combine_r(r_l(s[3]), 40..43)] = _pack(ret['my_rank']     , 4)
+        file[combine_r(r_l(s[3]), 44..47)] = _pack(ret['my_replay_id'], 4)
+        File.binwrite("nprofile", file)
+        print "."
+      end
+    }
+    print " patched.\n" unless !success
+  end
+  if types.empty? || types.include?(:ep_unscored_beaten)
+    print "Patching unscored beaten episodes..."
+    $eps[:unscored][:beaten].each{ |s|
+      ret = download(id, s[3], true)
+      if ret == -1 || !success
+        success = false
+        break
+      else
+        next if ret.nil?
+        file[combine_r(r_e(s[3]), 36..39)] = _pack(ret['my_score']    , 4)
+        file[combine_r(r_e(s[3]), 40..43)] = _pack(ret['my_rank']     , 4)
+        file[combine_r(r_e(s[3]), 44..47)] = _pack(ret['my_replay_id'], 4)
+        File.binwrite("nprofile", file)
+        print "."
+      end
+    }
+    print " patched.\n" unless !success
   end
 
-  File.binwrite("nprofile", file)
-  print("nprofile patched successfully, time: " + (1000 * (Time.now - t)).round(3).to_s + "ms.\n")
+  update_scores
+  message = success ? "successfully" : "partially"
+  print("nprofile patched #{message}, time: " + (1000 * (Time.now - t)).round(3).to_s + " ms.\n")
   return true
+end
+
+def print_scores
+  ids_l = {
+    "SI" => (   0.. 124).to_a,
+    "S"  => ( 600..1199).to_a,
+    "SU" => (2400..2999).to_a,
+    "SL" => (1200..1799).to_a,
+    "?"  => (1800..1919).to_a,
+    "!"  => (3000..3119).to_a
+  }
+  ids_e = {
+    "SI" => (  0.. 24).to_a,
+    "S"  => (120..239).to_a,
+    "SU" => (480..599).to_a,
+    "SL" => (240..359).to_a
+  }
+  levels = ids_l.map{ |tab, ids|
+    [tab, $lvls[:scored].map{ |k, v| v.select{ |l| ids.include?(l[3]) }.map{ |l| l[0] } }.flatten]
+  }.to_h
+  episodes = ids_e.map{ |tab, ids|
+    [tab, $eps[:scored].map{ |k, v| v.select{ |l| ids.include?(l[3]) }.map{ |l| l[0] } }.flatten]
+  }.to_h
+  tls = levels.map{ |tab, s| [tab, s.sum.to_f / 1000] }.to_h
+  tes = episodes.map{ |tab, s| [tab, s.sum.to_f / 1000] }.to_h
+  t = []
+  t << ["SCORES", "Levels", "Scores", "Episodes", "Scores"]
+  t << :sep
+  ids_l.each{ |tab, r|
+    t << [
+      tab.to_s,
+      ("%.3f" % tls[tab]).rjust(10, " "),
+      levels[tab].size.to_s.rjust(4, " ") + " / " + r.size.to_s.rjust(4, " "),
+      tes[tab].nil? ? "     0.000" : ("%.3f" % tes[tab]).rjust(10, " "),
+      episodes[tab].nil? ? "  0 /   0" : episodes[tab].size.to_s.rjust(3, " ") + " / " + ids_e[tab].size.to_s.rjust(3, " "),
+    ]
+  }
+  t << :sep
+  t << [
+    "Total",
+    ("%.3f" % tls.map{ |k, v| v }.sum).rjust(10, " "),
+    $lvls[:scored].reduce(0){ |sum, s| sum += s[1].size }.to_s.rjust(4, " ") + " / 2165",
+    ("%.3f" % tes.map{ |k, v| v }.sum).rjust(10, " "),
+    $eps[:scored].reduce(0){ |sum, s| sum += s[1].size }.to_s.rjust(3, " ") + " / 385",
+  ]
+  puts make_table(t)
 end
 
 def main
@@ -299,6 +411,8 @@ def main
           puts "nprofile file needs to be on the script's folder, and with that name."
           return
         end
+      when "scores"
+        print_scores
       else
         print_usage
     end
