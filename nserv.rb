8fc3f637af576b63cbc9ceb783e2f154e8130ce9
edelkas <edlucasma@gmail.com>
2023-01-09 02:23:42 +0100

    proxy connected with outte, injection working!

--- nserv.rb
+++ nserv.rb
@@ -8,21 +8,47 @@ require 'socket'
 #   block and header, ready to be dumped in the final file.
 # - Use actual request to deduce mode and tab, so that we set it correctly to
 #   inject it wherever the user is.
+# - What happens when you switch userlevel tabs very quickly? (sockets closing, etc).
+# - Implement page browsing in-game (i.e., instead of having to reinject each page,
+#   we send chunks of 25, but when you get to the bottom, the game requests the next
+#   page, and we parse that.
 
 EXPORT     = false # Export raw HTTP requests and responses, for debugging
 EXPORT_REQ = false
 EXPORT_DBG = false
 EXPORT_RES = false
 INTERCEPT  = true  # Whether to intercept or forward userlevel requests
+TEST       = true  # Use test outte (at localhost)
 
-$port   = 8124
-$target = "https://dojo.nplusplus.ninja"
-$proxy  = "http://localhost:#{$port}".ljust($target.length, "\x00")
-$socket = nil
-$count  = 0
+$port_npp      = 8124
+$port_outte    = 8125
+$target        = "https://dojo.nplusplus.ninja"
+$proxy         = "http://localhost:#{$port_npp}".ljust($target.length, "\x00")
+$outte         = TEST ? "127.0.0.1" : "45.32.150.168"
+$timeout_npp   = 1
+$timeout_outte = 5
+$socket        = nil
+$res           = nil
+$count         = 1
 
 def clear
   print "\r".ljust(80, ' ') + "\r"
+end
+
+def log(line)
+  method, path, protocol = line.split  
+  puts "#{"%-4s" % method} #{path.split('?')[0].split('/')[-1]}"
+end
+
+def _pack(n, size)
+  n.to_s(16).rjust(2 * size, "0").scan(/../).reverse.map{ |b|
+    [b].pack('H*')[0]
+  }.join.force_encoding("ascii-8bit")
+end
+
+def _unpack(bytes)
+  if bytes.is_a?(Array) then bytes = bytes.join end
+  bytes.unpack('H*')[0].scan(/../).reverse.join.to_i(16)
 end
 
 def find_lib
@@ -44,12 +70,16 @@ def depatch
   puts 'Depatched'
 end
 
-def read(client)
+def read(client, npp)
   req = ""
   begin
     req << client.read_nonblock(16 * 1024)
   rescue Errno::EAGAIN
-    retry if IO.select([client], nil, nil, 1)
+    if IO.select([client], nil, nil, npp ? $timeout_npp : $timeout_outte)
+      retry
+    else
+      return nil if req.size == 0
+    end
   rescue
   end
   req
@@ -65,14 +95,41 @@ def clear_headers(http)
   http
 end
 
-def log(line)
-  method, path, protocol = line.split  
-  puts "#{"%-4s" % method} #{path.split('?')[0].split('/')[-1]}"
+def empty_query(pars)
+  cat     = pars.key?('search') ? 36 : (pars['qt'].to_i || 10)
+  mode    = pars['mode'].to_i || 0
+  header  = Time.now.strftime("%Y-%m-%d-%H:%M") # Date of query  (16B)
+  header += _pack(0,    4)                      # Map count      ( 4B)
+  header += _pack(0,    4)                      # Query page     ( 4B)
+  header += _pack(0,    4)                      # ?              ( 4B)
+  header += _pack(cat,  4)                      # Query category ( 4B)
+  header += _pack(mode, 4)                      # Game mode      ( 4B)
+  header += _pack(5,    4)                      # ?              ( 4B)
+  header += _pack(500,  4)                      # ?              ( 4B)
+  header += _pack(0,    4)                      # ?              ( 4B)
+  header
+end
+
+def parse_params(path)
+  pars = path.split('?').last.split('&').map{ |p| p.split('=') }.to_h
+  pars.delete('steam_auth')
+  pars
+end
+
+# TODO: Add more integrity checks (map count, block lengths, etc)
+def validate_res(res, pars)
+  return empty_query(pars) if !res.is_a?(String)
+  return empty_query(pars) if res.size < 48
+  return empty_query(pars) if _unpack(res[32..35]) != pars['mode'].to_i
+  cat = pars.key?('search') ? 36 : (pars['qt'].to_i || 10)
+  res[28..31] = _pack(cat, 4)
+  res
 end
 
 def intercept(req)
   return forward(req) if !INTERCEPT
-  body = IO.binread('query')
+  pars = parse_params(req.split("\n")[0].split[1])
+  body = validate_res($res, pars)
   status = "HTTP/1.1 200 OK\r\n"
   headers = {
     'content-type'   => 'application/octet-stream',
@@ -117,19 +174,18 @@ end
 
 def startup
   patch
-  $socket = TCPServer.new($port)
+  $socket = TCPServer.new($port_npp)
   puts 'Started'
 end
 
 def loop
   client = $socket.accept
   req = client.gets
-  $count += 1
   log(req)
   method, path, protocol = req.split
-  req << read(client)
+  req << read(client, true).to_s
   IO.binwrite("req_#{$count}", req) if EXPORT || EXPORT_REQ
-  if method == 'GET' && path.split('?')[0].split('/')[-1] == 'query_levels'
+  if method == 'GET' && ['query_levels', 'levels'].include?(path.split('?')[0].split('/')[-1])
     res = intercept(req)
   else
     res = forward(req)
@@ -137,6 +193,12 @@ def loop
   IO.binwrite("res_#{$count}", res) if EXPORT || EXPORT_RES
   client.write(res)
   client.close
+  $count += 1
+rescue => e
+  puts "Unknown error."
+  puts e
+  puts e.backtrace.join("\n")
+  client.close if client.is_a?(BasicSocket)
 end
 
 def shutdown
@@ -146,6 +208,21 @@ def shutdown
   exit
 end
 
+def call(req)
+  Socket.tcp($outte, $port_outte) do |conn|
+    conn.write(req)
+    conn.close_write
+    $res = read(conn, false)
+    conn.close
+    puts($res.nil? ? "Connection to outte timed out." : "Received #{$res.size} bytes from outte.")
+  end
+rescue
+  puts "Unable to connect to outte."
+end
+
 trap 'INT' do shutdown end
 startup
-while true do loop end
+threads = {}
+threads[:server] = Thread.new{ loop while true }
+threads[:input]  = Thread.new{ call(STDIN.gets.chomp) while true }
+threads[:server].join
