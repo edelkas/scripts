09dcdf832f1fdf5a712236aea86a5e6367014712
edelkas <edlucasma@gmail.com>
2025-03-21 00:12:33 +0100

    generate_palette: more robust, complete

--- generate_palette.rb
+++ generate_palette.rb
@@ -1,34 +1,60 @@
 # This was used to generate the palette image
 # It's not a complete TGA parser, just the bare minimum required
 require 'chunky_png'
-include ChunkyPNG::Color
 
-dir = '.steam/steam/steamapps/common/N++/NPP/Palettes'
-palettes = Dir.entries(dir).reject{ |f| f == '.' || f == '..' }.sort_by(&:downcase)
-print(palettes.to_s)
-names = ["background", "ninja", "entityMine", "entityGold", "entityDoorExit",
-  "entityDoorExitSwitch", "entityDoorRegular", "entityDoorLocked",
-  "entityDoorTrap", "entityLaunchPad", "entityOneWayPlatform",
-  "entityDroneChaingun", "entityDroneLaser", "entityDroneZap",
-  "entityDroneChaser", "entityFloorGuard", "entityBounceBlock",
-  "entityRocket", "entityTurret", "entityThwomp", "entityEvilNinja",
-  "entityDualLaser", "entityBoostPad", "entityBat",
-  "entityEyeBat", "entityShoveThwomp"].map{ |n| n + ".tga" }
-output = ChunkyPNG::Image.new(91, palettes.size, WHITE)
+HEADER_SIZE = 18
+FILE_NAMES = [
+  # Main map colors used for screenshots
+  "background",        "ninja",                "entityMine",           "entityGold",
+  "entityDoorExit",    "entityDoorExitSwitch", "entityDoorRegular",    "entityDoorLocked",
+  "entityDoorTrap",    "entityLaunchPad",      "entityOneWayPlatform", "entityDroneChaingun",
+  "entityDroneLaser",  "entityDroneZap",       "entityDroneChaser",    "entityFloorGuard",
+  "entityBounceBlock", "entityRocket",         "entityTurret",         "entityThwomp",
+  "entityEvilNinja",   "entityDualLaser",      "entityBoostPad",       "entityBat",
+  "entityEyeBat",      "entityShoveThwomp",
+
+  # Colors for other parts of the gameplay
+  "headbands",         "explosions",           "timeBar",              "timeBarRace",
+  "fxNinja",           "fxDroneZap",           "fxFloorguardZap",
+
+  # Interface colors
+  "menu",              "editor"
+]
+DIR = '~/.steam/steam/steamapps/common/N++/NPP/Palettes'
+
+# Parse palettes and count total colors to define output image
+puts "Generating palette image..."
+palettes = Dir.entries(DIR).reject{ |f| f == '.' || f == '..' }.sort_by(&:downcase)
+colors = FILE_NAMES.inject(0){ |total, name|
+  file = File.binread("#{DIR}/#{palettes.first}/#{name}.tga")
+  total + file[12, 2].unpack('S<')[0] / 64
+}
+output = ChunkyPNG::Image.new(colors, palettes.size, ChunkyPNG::Color::WHITE)
+puts "Palettes found: #{palettes.size}"
+puts "Colors found: #{colors}"
+
+# Parse all colors and fill output image
+count = palettes.count
 palettes.each_with_index{ |palette, y|
-  x = 0
-  names.each{ |name|
-    file = File.binread(dir + "/" + palette + "/" + name)
-    width = file[12..13].reverse.unpack('H*')[0].to_i(16)
+  print("Parsing palette [#{y + 1} / #{count}] #{palette}".ljust(80) + "\r")
+  x = -1
+
+  FILE_NAMES.each{ |name|
+  	# Parse TGA properties
+    file = File.binread("#{DIR}/#{palette}/#{name}.tga")
+    ox, oy, width, height, depth, desc = file[8, 10].unpack('S<4C2')
     colors = width / 64
-    initial = 18 + 4 * 32 * width + 4 * 32
-    step = 4 * 64
-    (0..colors - 1).each{ |i|
-      color_code = file[initial + step * i .. initial + step * i + 3]
-      color = (color_code[0..2].reverse + color_code[3]).unpack('H*')[0]
-      output[x,y] = ChunkyPNG::Color.from_hex(color)
-      x += 1
+    channels = depth / 8
+
+    # We sample the "middle" pixel (32, 32) of each 64x64 block
+    initial = HEADER_SIZE + (32 * channels) * (width + 1)
+    step = 64 * channels
+    colors.times.each{ |i|
+      color = file[initial + step * i, 3].reverse.unpack('H*')[0]
+      output[x += 1, y] = ChunkyPNG::Color.from_hex(color)
     }
   }
 }
-output.save('palette.png')
+puts "Done".ljust(80)
+
+output.save('palette.png', :fast_rgb)
